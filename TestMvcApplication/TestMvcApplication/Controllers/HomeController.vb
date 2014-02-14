048ea681c84f21847cec54265d7bd250f730a286
josegeorge <jose.george@sterlingventures.net>
2014-02-14 10:02:21 +0000

    Commit

--- TestMvcApplication/TestMvcApplication/Controllers/HomeController.vb
+++ TestMvcApplication/TestMvcApplication/Controllers/HomeController.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        ViewData("Message") = "Modify this template to jump-start your ASP.NET MVC application."
+        ViewData("Message") = "This is testing Project.Modify this template to jump-start your ASP.NET MVC application."
 
         Return View()
     End Function
